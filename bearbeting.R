d58a3ee43ef1e36c69af219b0f09a7739545341d
OskarStorberg <88612052+Oggestor@users.noreply.github.com>
2023-03-09 12:39:54 +0100

    Update bearbeting.R

--- bearbeting.R
+++ bearbeting.R
@@ -207,15 +207,31 @@ for(j in 1:length(bolag1till5)){
   }else{
     i <- 4
   }
+    
+    df[df$number == bolag1till5[j] & (df$y5 == "1" | df$y5 == "2" |
+                                    df$y3 == "1" | df$y3 == "2" |
+                                    df$y2 == "1" | df$y2 == "2"),i] <- "nej"
+  
+  df[df$number == bolag1till5[j] & (df$y5 == "4" | df$y5 == "5" |
+                                    df$y3 == "4" | df$y3 == "5" |
+                                    df$y2 == "4" | df$y2 == "5"),i] <- "ja"
   
   
-  df[df$number == bolag1till5[j] & (df$y5 == "1" | df$y5 == "2" |
-                                      df$y3 == "1" | df$y3 == "2" |
-                                      df$y2 == "1" | df$y2 == "2"),i] <- "nej"
+  df[df$number == bolag1till5[j] & 
+       (df$y5 == "3" | df$y5 == "-1" | df$y5 == "6"|
+        df$y3 == "3" | df$y3 == "-1" | df$y3 == "6"|
+        df$y2 == "3" | df$y2 == "-1" | df$y2 == "6"),i] <- "vet ej"
   
-  df[df$number == bolag1till5[j] & (df$y5 == "4" | df$y5 == "5" | df$y5 == "3" | df$y5 == "-1" | df$y5 == "6"|
-                                      df$y3 == "4" | df$y3 == "5" | df$y3 == "3" | df$y3 == "-1" | df$y3 == "6"|
-                                      df$y2 == "4" | df$y2 == "5" | df$y2 == "3" | df$y2 == "-1" | df$y2 == "6"),i] <- "ja"
+  
+ # df[df$number == bolag1till5[j] & (df$y5 == "1" | df$y5 == "2" |
+   #                                   df$y3 == "1" | df$y3 == "2" |
+   #                                   df$y2 == "1" | df$y2 == "2"),i] <- "nej"
+  
+ # df[df$number == bolag1till5[j] & (df$y5 == "4" | df$y5 == "5" | df$y5 == "3" | df$y5 == "-1" | df$y5 == "6"|
+  #                                    df$y3 == "4" | df$y3 == "5" | df$y3 == "3" | df$y3 == "-1" | df$y3 == "6"|
+   #                                   df$y2 == "4" | df$y2 == "5" | df$y2 == "3" | df$y2 == "-1" | df$y2 == "6"),i] <- "ja"
+    
+    
   
   
   # df[df$number == bolag1till5[j] & 
@@ -239,15 +255,31 @@ for(j in 1:length(bolag1till2)){
   }else{
     i <- 4
   }
-  
-  
-  df[df$number == bolag1till2[j] & (df$y5 == "1"  | df$y5 == "3" | df$y5 == "4" | 
-                                      df$y3 == "1" |  df$y3 == "3" | df$y3 == "4" | 
-                                      df$y2 == "1" | df$y2 == "3" | df$y2 == "4"),i] <- "ja"
-  
-  df[df$number == bolag1till2[j] & (df$y5 == "2" | 
+    
+        df[df$number == bolag1till2[j] & (df$y5 == "1"  |
+                                      df$y3 == "1" | 
+                                      df$y2 == "1" ),i] <- "ja"
+    
+    df[df$number == bolag1till2[j] & (df$y5 == "2" | 
                                       df$y3 == "2" |
                                       df$y2 == "2" ),i] <- "nej"
+    
+    
+    df[df$number == bolag1till2[j] & 
+         (df$y5 == "3" | df$y5 == "4" | 
+          df$y3 == "3" | df$y3 == "4" | 
+          df$y2 == "3" | df$y2 == "4" ),i] <- "vet ej"
+    
+    
+  
+  
+ # df[df$number == bolag1till2[j] & (df$y5 == "1"  | df$y5 == "3" | df$y5 == "4" | 
+   #                                   df$y3 == "1" |  df$y3 == "3" | df$y3 == "4" | 
+   #                                   df$y2 == "1" | df$y2 == "3" | df$y2 == "4"),i] <- "ja"
+  
+  #df[df$number == bolag1till2[j] & (df$y5 == "2" | 
+    #                                  df$y3 == "2" |
+      #                                df$y2 == "2" ),i] <- "nej"
   
   
   # df[df$number == bolag1till2[j] & 
